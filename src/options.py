8395a9bf3eb8dec6915b00ffec2c7fbb74423839
Manav Kumar <mkumar097@gmail.com>
2020-12-01 07:34:46 -0500

    Fixed the indentation for some of the attributes that I wrote. Before they were inside the __init__ function.

--- src/options.py
+++ src/options.py
@@ -23,46 +23,46 @@ class Options:
         self._strike = strike
         self._option_price = option_price
 
-        @property
-        def current(self):
-            return self._current
+    @property
+    def current(self):
+        return self._current
 
-        @property
-        def strike(self):
-            return self._strike
+    @property
+    def strike(self):
+        return self._strike
 
-        @property
-        def option_price(self):
+    @property
+    def option_price(self):
+        return self._option_price
+
+    @property
+    def long_call(self) -> float:
+
+        if self._current > self._strike:
+            return self._current - self._strike - self._option_price
+        else:
+            return -self._option_price
+
+    @property
+    def long_put(self) -> float:
+
+        if self._strike > self._current:
+            return self._strike - self._current - self._option_price
+        else:
+            return -self._option_price
+
+    @property
+    def short_call(self) -> float:
+
+        if self._strike > self._current:
             return self._option_price
+        else:
+            return self._strike - self._current + self._option_price
 
-        @property
-        def long_call(self) -> float:
+    @property
+    def short_put(self) -> float:
 
-            if self._current > self._strike:
-                return self._current - self._strike - self._option_price
-            else:
-                return -self._option_price
-
-        @property
-        def long_put(self) -> float:
-
-            if self._strike > self._current:
-                return self._strike - self._current - self._option_price
-            else:
-                return -self._option_price
-
-        @property
-        def short_call(self) -> float:
-
-            if self._strike > self._current:
-                return self._option_price
-            else:
-                return self._strike - self._current + self._option_price
-
-        @property
-        def short_put(self) -> float:
-
-            if self._strike < self._current:
-                return self._option_price
-            else:
-                return self._current - self._strike + self._option_price
+        if self._strike < self._current:
+            return self._option_price
+        else:
+            return self._current - self._strike + self._option_price
